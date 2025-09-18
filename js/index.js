61e2865d798f19d98180bd67f3ea68e49f37a0c8
MohairSaeed <mohair.zylo@gmail.com>
2025-09-18 10:46:51 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 5;
+let speed = 19;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -29,6 +29,14 @@ function isCollide(snake) {
     for (let i = 1; i < snakeArr.length; i++) {
         if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
             return true;
+        }
+    }
+    // If you bump into the wall
+    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+        return true;
+    }
+        
+    return false;
 }
 
 function gameEngine(){
@@ -138,10 +146,4 @@ window.addEventListener('keydown', e =>{
             break;
     }
 
-
 });
-
-
-
-
-
